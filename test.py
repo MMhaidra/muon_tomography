3edc3df1064725d4f5f9e6ec0bed90c6c013e21a
Austin Schneider <hogenshpogen@gmail.com>
2017-04-27 15:51:27 -0500

    Initial commit.

---
+++ test.py
@@ -0,0 +1,49 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import geometry as geo
+import materials
+import tracking
+
+
+def test_surface():
+    # Test surface
+    N = 10000
+    surface = geo.hex_surface()
+    v_scale = 2.0
+    v = geo.Voxel(np.array([0, 0, 3]), v_scale)
+    i = 2
+    points = np.random.uniform(-v_scale*2, v_scale*2, (N, 3)) + np.array([0, 0, 3]) * v_scale
+    points[:,i] = v.center[i]
+    w = np.array([v.contains_point(p) for p in points])
+
+    plt.plot(points[w,(i+1)%3], points[w,(i+2)%3], color='b', marker='.', linestyle='')
+    plt.plot(points[w == False,(i+1)%3], points[w == False,(i+2)%3], color='r', marker='.', linestyle='')
+    plt.show()
+
+def test_geo():
+    # Test geometry
+    surface = geo.hex_surface()
+    root_node = geo.GeometryNode(surface)
+    g = geo.Geometry([root_node], voxel_scale=2.0)
+    #print len(geo.voxel_dict.values())
+    #print len([val for val in geo.voxel_dict.values() if len(val) > 0])
+    ray = geo.Ray(np.array([0, 0, -20]), np.array([0, 0, 1]))
+    print g.ray_trace(ray)
+    print g.ray_trace(ray, all_intersections=True)
+
+def test_propagation():
+    surface = geo.hex_surface()
+    root_node = geo.GeometryNode(surface)
+    root_node.properties = materials.get_properties(92)
+    g = geo.Geometry([root_node], voxel_scale=2.0)
+    position, direction = (np.array([0, 0, -20]), np.array([0, 0, 1]))
+    mass = tracking.muon_mass
+    energy = 1
+    track = tracking.Track(position, direction, mass, energy, g)
+    prop = tracking.Propagator(track)
+
+
+
+#test_surface()
+#test_geo()
+test_propagation()
