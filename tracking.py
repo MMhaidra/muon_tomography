Fixed cross section calculation. Fixed end conditions.

tracking.py
@@ -6,17 +6,16 @@ c = .299792458 # m/ns
 c2 = c**2 # m/ns
 a0 = 5.2917721067e-11 # m
 e2_epsilon0 = 1.8095128e-17 # (m*GeV)
-muon_mass_energy = .105658374 # (GeV)
-muon_mass = .105658374 / c2 # (GeV/(m/s)^2)
+muon_mass = .105658374 # (GeV)
 
 def gamma(energy, mass):
-    return energy/(mass*c2)
+    return energy/(mass)
 
 def beta(gamma):
-    return 1-1/gamma**2
+    return 1.-1./gamma**2.
 
 def screening_parameter(mass, gamma, beta, Z):
-    return 0.25*(hb/(mass*gamma*beta*c))**2/(0.885*Z**(1./3.)*a0)**2
+    return 0.25*(hb/(mass*gamma*beta/c))**2*(0.885*Z**(-1./3.)*a0)**(-2.)
 
 def G1W(A):
     return 2*A*((1+A)*np.log((1+A)/A)-1)
@@ -25,7 +24,7 @@ def G2W(A):
     return 6*A*(1+A)*((1+2*A)*np.log((1+A)/A)-2)
 
 def sigmaW(A, Z, mass, gamma, beta):
-    return (Z*e2_epsilon0)**2/(mass * gamma * beta**2 * c2)*np.pi/(A*(1+A))
+    return (Z*e2_epsilon0)**2/(mass * gamma * beta**2)**2*np.pi/(A*(1+A))
 
 def lambdaW(N, sigmaW):
     return 1/(N*sigmaW)
@@ -44,33 +43,33 @@ def mu_cut(A, lambdah, lambdaW):
 
 def mu_hard(A, mu_cut):
     xi = np.random.random()
-    return mu_cut + (A+mu_cut)*xi*(1-mu_cut)/((A+1)-xi*(1-mu_cut))
+    return mu_cut + (A+mu_cut)*xi*(1.-mu_cut)/((A+1.)-xi*(1.-mu_cut))
 
 def lambda1Ws(A, lambdaW, mu_cut):
-    return 2/lambdaW * A * (1+A)*(np.log((mu_cut+A)/A)-mu_cut/(mu_cut+A))
+    return 2./lambdaW * A * (1.+A)*(np.log((mu_cut+A)/A)-mu_cut/(mu_cut+A))
 
 def lambda2Ws(A, lambdaW, mu_cut):
-    return 6/lambdaW*A*(1+A)*((1+2*A)*np.log((mu_cut+A)/A)-(1+2*A+mu_cut)*mu_cut/(mu_cut+A))
+    return 6./lambdaW*A*(1.+A)*((1.+2.*A)*np.log((mu_cut+A)/A)-(1.+2.*A+mu_cut)*mu_cut/(mu_cut+A))
 
 def mu_soft_mean(t, lambda1Ws):
-    return 0.5*(1-np.exp(-t/lambda1Ws))
+    return 0.5*(1.-np.exp(-t/lambda1Ws))
 
 def mu2_soft_mean(t, mu_soft_mean, lambda2Ws):
-    return mu_soft_mean - 1/6*(1-np.exp(-t/lambda2Ws))
+    return mu_soft_mean - 1./6.*(1.-np.exp(-t/lambda2Ws))
 
 def a(mu_soft_mean, mu2_soft_mean):
-    return (mu2_soft_mean-mu_soft_mean**2)/(mu_soft_mean*(1-mu_soft_mean))
+    return (mu2_soft_mean-mu_soft_mean**2.)/(mu_soft_mean*(1.-mu_soft_mean))
 
 def alpha(a):
-    return (3*a+(a*(a+8))**0.5)/(4*(1-a))
+    return (3.*a+(a*(a+8.))**0.5)/(4.*(1.-a))
 
 def mu_soft(mu_soft_mean, alpha):
     xi = np.random.random()
-    comp = 1-mu_soft_mean
+    comp = 1.-mu_soft_mean
     if xi < comp:
         return mu_soft_mean*(xi/(comp))**alpha
     else:
-        return 1-comp*((1-xi)/mu_soft_mean)**alpha
+        return 1.-comp*((1.-xi)/mu_soft_mean)**alpha
 
 class Track:
     def __init__(self, position, direction, mass, energy, geometry):
@@ -81,10 +80,12 @@ class Track:
         self.geometry = geometry
         self.update()
     def update(self, intersections=None):
+        #print 'Track update'
         self.geo_node, self.intersections = self.geometry.get_containing_surface(self.ray, intersections, return_intersections=True)
-        if intersections is not None and len(intersections) > 0 and self.intersections[0][2] < 1e-6:
-            self.ray.p = self.ray.p + self.ray.v + (self.intersections[0][2]+1e-9)
+        if self.intersections is not None and len(self.intersections) > 0 and self.intersections[0][2] < 1e-6:
+            self.ray.p = self.ray.p + self.ray.d + (self.intersections[0][2]+1e-9)
             self.update(intersections=self.intersections[1:])
+
     def get_material_properties(self):
         if self.geo_node is None:
             props = self.geometry.default_properties
@@ -99,7 +100,7 @@ class Propagator:
         self.energy = track.energy
         self.Z = 0
         self.N = 0
-        self.Cs = 0.05
+        self.Cs = 1
         self.update_model()
         pass
 
@@ -136,39 +137,47 @@ class Propagator:
     def propagate_step(self):
         # 1: set the initial position and direction of the particle
         p0 = self.track.ray.p.copy()
-        v0 = self.track.ray.v.copy()
+        v0 = self.track.ray.d.copy()
+
+        #print 'Tracking step starts at:', p0, 'going', v0
 
         # Update the MSC model quantities is the energy has changed by 1% or more
         if 1-self.track.energy/self.energy >= 0.01:
             self.update_model(material_update=False, energy_update=True)
 
+        #print np.arccos(1-2*self.mu_cut)/np.pi, self.A, self.lambdah, self.lambdaW, self.sigmaW
+
         # 2: sample the length t of the step for the hard scatter using t=-lambdah*ln(xi)
         xi = np.random.random()
         self.t = -self.lambdah*np.log(xi)
 
+        #print 'Hard scatter after', self.t, 'm'
+
         # 3: advance the particle tau=t*xi
         xi = np.random.random()
         self.tau = self.t*xi
+        #print 'Soft scatter after', self.tau, 'm'
         p1 = p0 + v0*self.tau
 
         # 4: check if the track has crossed an interface
-        intersections = geometry.ray_trace(ray, all_intersections=False)
+        intersections = self.track.geometry.ray_trace(self.track.ray, all_intersections=False)
         crossed = False
         if len(intersections) > 0:
             geo_lim = intersections[0][2]
             if geo_lim <= self.tau and geo_lim > 1e-9:
+                #print 'Hit the geometric limit before soft scatter', geo_lim, self.tau
                 # If the track crosses an interface, stop it at the interface
                 self.track.ray.p = p0 + v0*geo_lim
                 self.track.update()
                 self.update_model(material_update=True, energy_update=False)
-                return
+                return not ((self.track.geo_node is None or self.track.geo_node.level >= -1) and len(self.track.intersections) == 0)
         self.track.ray.p = p1
 
         # 5: simulate the soft scattering
         self.mu_soft_mean = mu_soft_mean(self.t, self.lambda1Ws)
         self.mu2_soft_mean = mu2_soft_mean(self.t, self.mu_soft_mean, self.lambda2Ws)
         self.a = a(self.mu_soft_mean, self.mu2_soft_mean)
-        self.alpha = alpha(a)
+        self.alpha = alpha(self.a)
         self.mu_soft = mu_hard(self.A, self.mu_cut)
         self.theta_soft = np.arccos(1-2*self.mu_soft)
         xi = np.random.random()
@@ -184,22 +193,28 @@ class Propagator:
         # Step2: perform rotation
         # Step3: project back onto original coordinate system
         v1 = geo.deflect_vector(v0, self.theta_soft, self.phi_soft)
-        self.track.ray.v = v1
+        self.track.ray.d = v1
 
         # 6: advance the particle by t-tau
         p2 = p1 + v1*(self.t - self.tau)
         
         # 7: check if the track has crossed an interface
-        intersections = geometry.ray_trace(self.track.ray, all_intersections=False)
+        intersections = self.track.geometry.ray_trace(self.track.ray, all_intersections=False)
         crossed = False
         if len(intersections) > 0:
             geo_lim = intersections[0][2]
             if geo_lim <= (self.t - self.tau) and geo_lim > 1e-9:
+                #print 'Hit the geometric limit before hard scatter', geo_lim, self.t-self.tau
                 # If the track crosses an interface, stop it at the interface
+                #print 'Moving track to boundary before hard scatter'
                 self.track.ray.p = p1 + v1*geo_lim
+                #print 'Updating track'
                 self.track.update()
+                #print 'Updating model'
                 self.update_model(material_update=True, energy_update=False)
-                return
+                #print 'Returning'
+                #return self.track.geo_node is not None and self.track.geo_node.level >= -1
+                return not ((self.track.geo_node is None or self.track.geo_node.level >= -1) and len(self.track.intersections) == 0)
         self.track.ray.p = p2
 
         # 8: simulate a hard scatter
@@ -210,4 +225,5 @@ class Propagator:
 
         # Change the direction of the vector
         v2 = geo.deflect_vector(v1, self.theta_hard, self.phi_hard)
-        self.track.ray.v = v2
+        self.track.ray.d = v2
+        return True